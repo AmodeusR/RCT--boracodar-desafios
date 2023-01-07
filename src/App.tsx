faa83067e12323079a93d82618c3e475420979d0
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-01-07 19:28:12 -0300

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import "./styles/home.scss";
+import MainPage from "./components/MainPage/MainPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage";
+import MusicPlayer from "./challenges/MusicPlayer";
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <MainPage />,
+  errorElement: <NotFoundPage />
+}, {
+  path:"music-player",
+  element: <MusicPlayer />
+}]);
 
 function App() {
 
   return (
-    <main className="home">
-      <h1 className="home__title">Desafios <span className="destaque">#boracodar</span></h1>
-      <p className="home__subtitle">Desafios semanais lançados pela <a href="https://rocketseat.com.br/" target="_blank">Rocketseat</a></p>
-      
-    </main>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
