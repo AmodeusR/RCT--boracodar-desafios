958ef93fbea543a91ec060f69f6ce44d6b0b9d3e
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-01-14 21:36:48 -0300

    Index for images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,12 @@
+import OTerno from "./o-terno.webp";
+import EsdrasGondim from "./esdras-gondim.webp";
+import EzoshikaGourmet from "./ezoshika-gourmet-club.webp";
+
+import MusicPlayerCover from "./challenges/music-player.png";
+
+export {
+  OTerno,
+  EsdrasGondim,
+  EzoshikaGourmet,
+  MusicPlayerCover
+}
