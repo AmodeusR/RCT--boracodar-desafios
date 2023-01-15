34277789ff35f0deba7746cd6d1c42668c217d99
Ricardo Magalhães <ricardogleiss@hotmail.com>
2023-01-14 21:36:03 -0300

    Index for components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Challenges } from "./Challenges/Challenges";
+export { default as MainPage } from "./MainPage/MainPage";
+export { default as NotFoundPage } from "./NotFoundPage/NotFoundPage";
+export { default as ChallengeCard } from "./ChallengeCard/ChallengeCard";
